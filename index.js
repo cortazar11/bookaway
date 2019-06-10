6753742db65838062133d7b74833fcb1a21f7362
Miguel Martinez <miguel.martinez11@gmail.com>
2019-06-10 16:29:11 +0200

    Preparing to deploy to heroku

--- index.js
+++ index.js
@@ -6,5 +6,15 @@ app.get('/',(req,res)=>{
     res.send('first app check')
 })
 
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+
+    const path= require('path')
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 const PORT= process.env.PORT || 5000
 app.listen(PORT)
